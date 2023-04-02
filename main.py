6817c1c8235d6c6329e79d5804473513b8210076
Tomass Abzalons <125128863+221RDC029@users.noreply.github.com>
2023-04-02 16:19:47 +0000

    Test

--- main.py
+++ main.py
@@ -13,19 +13,28 @@ def read_queries():
 def write_responses(result):
     print('\n'.join(result))
 
+# Izveidoju jaunu klasi phone_book, kur saglabāšu savas vērtības un veikšu risinājumus
+# lai prasītās vērtības atrastu
 class phone_book:
     def __init__(self):
         self.contacts = {}
 
+    # Šajā metodē tiks veikta telefona numura un vārda pievienošana telefona grāmatai
     def pievieno(self, number, name):
         self.contacts[number] = name
 
+    # Šajā metodē tiks veikta telefona vārda atrašana, pēc ievadītā numura
+    # Ja tāds numurs eksistē, tad tiks izvadīts tās personas vārds, kuram pieder šis telefona numurs
+    # Ja neeksistē, tad tiks izvadīts "not found"
     def atrod(self, number):
         if number in self.contacts:
             return self.contacts[number]
         else:
             return "not found"
 
+    # Šajā metodē tiks veikta personas vārda dzēšana
+    # Ja telefona ievadītais telefona numurs eksistē telefona grāmatā, tad vārds tiek izdzēsts
+    # Ja neeksistē, tad vienkārši dod atpakaļ False un nekas nenotiek
     def izdzēst(self, number):
         if number in self.contacts:
             del self.contacts[number]
@@ -37,11 +46,15 @@ def process_queries(queries):
     result = []
     kontaktu_grāmata = phone_book()
     for cur_query in queries:
+        # Ja ievadītais vārds ir "add", tad izsauc pievienošanas metodi
         if cur_query.type == 'add':
             kontaktu_grāmata.pievieno(cur_query.number, cur_query.name)
+        # Ja ievadītasis vārds sākumā ir "del", tad izsauc dzēšanas metodi
         elif cur_query.type == 'del':
             kontaktu_grāmata.izdzēst(cur_query.number)
         else:
+        # Trešā metode, kura paliek pāri ir atrast numuru, tāpēc, ja tiks ievadīts "find", tad tiks izsaukta metode
+        # atrod un tiks izvadīts prasītais vārds
             numurs = cur_query.number
             atrast_numuru = kontaktu_grāmata.atrod(numurs)
             result.append(atrast_numuru)
